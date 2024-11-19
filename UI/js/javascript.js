2c2efce9de1d7e47e6390356132ba409b796da5c
COMMUNITECT\thendershot <thendershot@solutionreach.com>
2024-11-18 17:29:39 -0700

    added html pages for the nav bar and filled them out considerably

--- UI/js/javascript.js
+++ UI/js/javascript.js
@@ -7,18 +7,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-//Adds underline for a clicked link in the top navbar
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('.nav-links a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            // Remove the active class from all links
-            navLinks.forEach(nav => nav.classList.remove('active'));
-
-            // Add the active class to the clicked link
-            link.classList.add('active');
-        });
-    });
-});
